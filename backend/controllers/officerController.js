c338cf02e23a6cbd3b14e09172c8016ca49c5ed7
Dikshit Yadav <2004dikshityadav@gmail.com>
2025-09-16 15:30:18 +0530

    pending

--- backend/controllers/officerController.js
+++ backend/controllers/officerController.js
@@ -39,8 +39,6 @@ const getOfficerDocuments = async (req, res) => {
 //   }
 // };
 
-
-
 const uploadSignature = async (req, res) => {
   try {
     if (!req.file) return res.status(400).json({ error: "No file uploaded" });
