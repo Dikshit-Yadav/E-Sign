b4e00a9842a859b1bd7b75d20e873f72965a2a4d
Dikshit Yadav <2004dikshityadav@gmail.com>
2026-03-23 10:52:30 +0530

    officer controller redis update

--- backend/routes/officer.js
+++ backend/routes/officer.js
@@ -6,7 +6,7 @@ const { getOfficerDocuments, uploadSignature} = require("../controllers/officerC
 const { UpdateSignDocument, rejectDocument} = require("../controllers/documentController");
 
 
-router.get("/documents",cache(`documents:${req.cookies.userId}`),  getOfficerDocuments);
+router.get("/documents",cache((req)=>`documents:${req.cookies.userId}`),  getOfficerDocuments);
 router.put("/documents/:id/sign", UpdateSignDocument);
 router.put("/documents/:id/reject", rejectDocument);
 router.post("/upload-signature", upload.single("signature"), uploadSignature);
